e3f528669b914808ddf1f55d1099739d51258a9c
Nonthawath Patchakapati <npc2540@hotmail.com>
2024-03-23 13:57:09 +0700

    add: javascripts values

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let firstName = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruay" 
+
+const vendorName = "Chef's Table by Chef James"
+const netPricePerBill = 15000
+const totalActiveVendor = 1500
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+netPricePerBill = 5000
+totalActiveVendor = 2500
